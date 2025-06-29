bb9ab30f1965f06a794f2bf273789d4679ee5e3d
iRyuch <jc.cardosoo@hotmail.com>
2025-06-29 18:00:57 -0300

    Delete siteTrabalho directory

--- siteTrabalho/index.js
+++
@@ -1,7 +0,0 @@
-let botao = document.querySelector('.botao');
-
-let mensagem = document.querySelector(".mensagem");
-
-botao.addEventListener("click", e =>{
-    mensagem.textContent="Obrigado por ler um pouco sobre mim, bom semestre!";
-});
