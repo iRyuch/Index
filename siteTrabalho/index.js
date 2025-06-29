f22515e541d39945442f45e867268b969f285ab1
iRyuch <jc.cardosoo@hotmail.com>
2025-06-29 17:41:08 -0300

    Update index.js

---
+++ siteTrabalho/index.js
@@ -0,0 +1,7 @@
+let botao = document.querySelector('.botao');
+
+let mensagem = document.querySelector(".mensagem");
+
+botao.addEventListener("click", e =>{
+    mensagem.textContent="Obrigado por ler um pouco sobre mim, bom semestre!";
+});
